d9ffd0238f80e60b4b208414c6d195b748a6d57c
Gayoma <Gayoma@GAYOMAYUSI>
2025-11-20 21:15:51 +0800

    Creating SqlQuery in C#

--- FrmClubRegistration.cs
+++ FrmClubRegistration.cs
@@ -13,7 +13,81 @@ namespace _07LaboratoryExercise
     public partial class FrmClubRegistration : Form
     {
 
-      
+        private ClubRegistrationQuery clubRegistrationQuery = new ClubRegistrationQuery();
+        private static int ID, Age, count;
+        private string FirstName, MiddleName, LastName, Gender, Program;
+        private long StudentID;
+
+        public FrmClubRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshListOfClubMembers();
+        }
+
+        private void FrmClubRegistration_Load(object sender, EventArgs e)
+        {
+   
+            RefreshListOfClubMembers();
+        }
+
+
+        public void RefreshListOfClubMembers()
+        {
+
+  
+            clubRegistrationQuery.DisplayList();
+
+
+            dgvClubMembers.DataSource = clubRegistrationQuery.bindingSource;
+
+        }
+
+        public int RegistrationID()
+        {
+            count++;
+            return count;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+            ID = RegistrationID();
+
+            StudentID = Convert.ToInt32(txtStudentID.Text);
+
+            FirstName = txtFirstName.Text;
+            MiddleName = txtMiddleName.Text;
+            LastName = txtLastName.Text;
+
+            Gender = cbGender.Text;
+            Program = cbProgram.Text;
+
+            Age = Convert.ToInt32(txtAge.Text);
+
+
+            clubRegistrationQuery.RegisterStudent(ID, StudentID, FirstName, MiddleName, LastName, Age, Gender, Program);
+            RegistrationID();
+
+
+
+        }
+
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            FrmUpdateMember frmUpdateMember = new FrmUpdateMember();
+            frmUpdateMember.Show();
+        }
+
        
        
     }
